cf32c614b388e3b91b194c610e59ee4bd80bd19e
Kuyen <iamcxa@gmail.com>
2017-02-23 10:15:39 +0800

    rename Evernote to evernote

--- facebook/webview.js
+++ facebook/webview.js
@@ -1,3 +1,15 @@
 module.exports = (Franz, options) => {
-    
+	const getValueById = (id) => {
+		return document.getElementById(id).innerText;
+	};
+	const getMessages = () => {
+		const unreadNotices = getValueById('notificationsCountValue');
+		const unreadMessage = getValueById('mercurymessagesCountValue');
+		Franz.setBadge(unreadMessage, unreadNotices);
+	}
+	Franz.loop(getMessages);
+	// Franz.injectCSS(path.join(__dirname, "style.css"));
+	document.getElementById('rightCol').remove();
+	document.getElementById('stream_pagelet').style.width = '150%';
+	document.getElementById('mainContainer').style.paddingLeft = '100px';
 }
--- github/webview.js
+++ github/webview.js
@@ -1,3 +1,10 @@
 module.exports = (Franz, options) => {
-    
+	const getMessages = () => {
+		const unread = document.getElementsByClassName('unread').innerText;
+		const hasUnread = typeof unread !== 'undefined';
+    console.log(unread, hasUnread);
+		const unreadMessage = hasUnread ? 1: 0;
+		Franz.setBadge(unreadMessage);
+	}
+	Franz.loop(getMessages);
 }
