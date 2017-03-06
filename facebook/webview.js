e392c6daf966333404968a32cbde3bca80ddf2c1
Kuyen <iamcxa@gmail.com>
2017-03-06 14:40:57 +0800

    feature - add facebook messager and wall msg notifications.

--- facebook/webview.js
+++ facebook/webview.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = (Franz, options) => {
 	const getValueById = (id) => {
 		return document.getElementById(id).innerText;
@@ -8,8 +10,4 @@ module.exports = (Franz, options) => {
 		Franz.setBadge(unreadMessage, unreadNotices);
 	}
 	Franz.loop(getMessages);
-	// Franz.injectCSS(path.join(__dirname, "style.css"));
-	document.getElementById('rightCol').remove();
-	document.getElementById('stream_pagelet').style.width = '150%';
-	document.getElementById('mainContainer').style.paddingLeft = '100px';
 }
